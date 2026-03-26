498c39b9a09edab882e23e75bbc0b8d638e6f02b
Nongjinguang <981794167@qq.com>
2026-03-26 16:30:24 +0800

    frontend

---
+++ src/test/java/com/yizhaoqi/smartpai/njg/aabb.java
@@ -0,0 +1,21 @@
+package com.yizhaoqi.smartpai.njg;
+
+public class aabb {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+}
+}
