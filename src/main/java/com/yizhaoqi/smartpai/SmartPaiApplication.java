a5dbdf4bf9a071bc4de8f42830930e0a398511c5
LIXINXING <2820265153@qq.com>
2026-03-27 19:11:41 +0800

    2.0

--- src/main/java/com/yizhaoqi/smartpai/SmartPaiApplication.java
+++ src/main/java/com/yizhaoqi/smartpai/SmartPaiApplication.java
@@ -9,5 +9,4 @@ public class SmartPaiApplication {
     public static void main(String[] args) {
         SpringApplication.run(SmartPaiApplication.class, args);
     }
-
 }
--- src/test/java/com/yizhaoqi/smartpai/SmartPaiApplicationTests.java
+++ src/test/java/com/yizhaoqi/smartpai/SmartPaiApplicationTests.java
@@ -5,9 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SmartPaiApplicationTests {
+//test
 
     @Test
     void contextLoads() {
     }
-
 }
